e9287c17207129d1c25052bee37d83fc87637d85
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-30 18:12:23 +0300

    доделал, кроме переключения картинок клавишами

--- js/great-gallery.js
+++ js/great-gallery.js
@@ -1,21 +1,57 @@
 import collection from "../gallery-items.js"
 
-const gallery = document.querySelector(".js-gallery")
+const galleryEl = document.querySelector(".js-gallery")
+const modalEl = document.querySelector(".js-lightbox")
+const btnCloseModal = document.querySelector("[data-action]")
+const imgModal = document.querySelector(".lightbox__image")
+const cardsMarkup = greatCardsMarkup(collection)
+const backdrop = document.querySelector("div.lightbox__overlay")
 
-const greatElementItem = collection
-	.map(({ original, preview, description }) => {
-		const creatElement = `<li class="gallery__item">
+galleryEl.insertAdjacentHTML("afterbegin", cardsMarkup)
+btnCloseModal.addEventListener("click", onCloseModalClick)
+backdrop.addEventListener("click", closeOnBackdrop)
+function greatCardsMarkup(collection) {
+	return collection
+		.map(({ preview, original, description }) => {
+			return `<li class="gallery__item">
     <a class="gallery__link"
-    href=${original}>
+    href="${original}">
     <img class="gallery__image"
-      src=${preview}
-      data-source=${original}
-      alt=${description}/>
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
   </a>
 </li>`
+		})
+		.join("")
+}
 
-		return creatElement
-	})
-	.join("")
+galleryEl.addEventListener("click", onPalettGalleryClick)
+function onPalettGalleryClick(evt) {
+	if (!evt.target.classList.contains("gallery__image")) {
+		return evt.target.dataset.source
+	}
+	evt.preventDefault()
+	modalEl.classList.add("is-open")
+	imgModal.src = evt.target.dataset.source
+	window.addEventListener("keydown", onEscCloseModal)
+}
 
-gallery.insertAdjacentHTML("afterbegin", greatElementItem)
+function onCloseModalClick(evt) {
+	modalEl.classList.remove("is-open")
+	imgModal.src = ""
+	window.removeEventListener("keydown", onEscCloseModal)
+}
+
+function closeOnBackdrop(evt) {
+	if (evt.target === evt.currentTarget) {
+		onCloseModalClick(evt)
+	}
+}
+
+function onEscCloseModal(evt) {
+	if (evt.code === "Escape") {
+		onCloseModalClick(evt)
+	}
+	console.log(evt.code)
+}
