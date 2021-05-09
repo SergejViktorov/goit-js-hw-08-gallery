a0dc451012ffb3140099fd1ece623539e1368a2f
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-05-09 17:16:09 +0300

    Update great-gallery.js

--- great-gallery.js
+++ great-gallery.js
@@ -1,46 +1,48 @@
-import collection from "./gallery-items.js"
+import collection from './gallery-items.js'
 
-const galleryEl = document.querySelector(".js-gallery")
-const modalEl = document.querySelector(".js-lightbox")
-const btnCloseModal = document.querySelector("[data-action]")
-const imgModal = document.querySelector(".lightbox__image")
+const galleryEl = document.querySelector('.js-gallery')
+const modalEl = document.querySelector('.js-lightbox')
+const btnCloseModal = document.querySelector('[data-action]')
+const imgModal = document.querySelector('.lightbox__image')
+const backdrop = document.querySelector('div.lightbox__overlay')
 const cardsMarkup = greatCardsMarkup(collection)
-const backdrop = document.querySelector("div.lightbox__overlay")
 
-galleryEl.insertAdjacentHTML("afterbegin", cardsMarkup)
-btnCloseModal.addEventListener("click", onCloseModalClick)
-backdrop.addEventListener("click", closeOnBackdrop)
+galleryEl.insertAdjacentHTML('afterbegin', cardsMarkup)
+btnCloseModal.addEventListener('click', onCloseModalClick)
+backdrop.addEventListener('click', closeOnBackdrop)
+galleryEl.addEventListener('click', onPalettGalleryClick)
+
 function greatCardsMarkup(collection) {
 	return collection
-		.map(({ preview, original, description }) => {
+		.map(({ preview, original, description }, index) => {
 			return `<li class="gallery__item">
     <a class="gallery__link"
     href="${original}">
     <img class="gallery__image"
       src="${preview}"
       data-source="${original}"
-      alt="${description}"/>
+      alt="${description}"
+	  data-index="${index}">
   </a>
 </li>`
 		})
-		.join("")
+		.join('')
 }
 
-galleryEl.addEventListener("click", onPalettGalleryClick)
 function onPalettGalleryClick(evt) {
-	if (!evt.target.classList.contains("gallery__image")) {
+	if (!evt.target.classList.contains('gallery__image')) {
 		return evt.target.dataset.source
 	}
 	evt.preventDefault()
-	modalEl.classList.add("is-open")
+	modalEl.classList.add('is-open')
 	imgModal.src = evt.target.dataset.source
-	window.addEventListener("keydown", onEscCloseModal)
+	window.addEventListener('keydown', onEscCloseModal)
 }
 
 function onCloseModalClick(evt) {
-	modalEl.classList.remove("is-open")
-	imgModal.src = ""
-	window.removeEventListener("keydown", onEscCloseModal)
+	modalEl.classList.remove('is-open')
+	imgModal.src = ''
+	window.removeEventListener('keydown', onEscCloseModal)
 }
 
 function closeOnBackdrop(evt) {
@@ -50,7 +52,7 @@ function closeOnBackdrop(evt) {
 }
 
 function onEscCloseModal(evt) {
-	if (evt.code === "Escape") {
+	if (evt.code === 'Escape') {
 		onCloseModalClick(evt)
 	}
 }
